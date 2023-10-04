1a05466079e6147ae096f1a546722aed3612d501
Facundo Grosso <facugrossovittore@gmail.com>
2023-10-03 22:29:23 -0300

    Primer commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete: EventEmitter<number>= new EventEmitter();
+
+  onDeleteCharacter(index:number):void{
+    this.onDelete.emit(index);
+  }
+
+  @Output()
+  public deleteCharacter: EventEmitter<string> = new EventEmitter();
+
+  deleteCharacterById(id?:string):void{
+    if (!id) return;
+    this.deleteCharacter.emit(id);
+  }
+
+
+}
